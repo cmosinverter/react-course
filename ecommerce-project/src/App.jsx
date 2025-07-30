0a9e7e4b8f9f3914900e84c94ad7bcaa0bc7afcb
cmosinverter <kevin95120@gmail.com>
2025-07-30 20:36:19 +0800

    Move all pages to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage.jsx';
 import { CheckoutPage } from './pages/CheckoutPage.jsx';
+import { OrdersPage } from './pages/OrdersPage.jsx';
+import { TrackingPage } from './pages/TrackingPage.jsx';
 import './App.css'
 
 function App() {
   return (
     <Routes>
-      <Route index element={<HomePage />}></Route>
-      <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
     
   )
