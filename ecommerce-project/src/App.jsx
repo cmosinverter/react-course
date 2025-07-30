8fd5bb65a00b90e6204d02d9e75b93b1d97b5015
cmosinverter <kevin95120@gmail.com>
2025-07-30 20:26:34 +0800

    Update links to match the router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,15 @@
-import { HomePage } from './pages/HomePage.jsx'
+import { Routes, Route } from 'react-router';
+import { HomePage } from './pages/HomePage.jsx';
+import { CheckoutPage } from './pages/CheckoutPage.jsx';
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />}></Route>
+      <Route path="checkout" element={<CheckoutPage />}></Route>
+    </Routes>
+    
   )
 }
 
--- ecommerce-project/src/main.jsx
+++ ecommerce-project/src/main.jsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter } from 'react-router';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-        <Route path="checkout" element={<div>Test Checkout Page</div>} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 )
