63f856009a9fb957e21777f9f288b4f76248a980
cmosinverter <kevin95120@gmail.com>
2025-07-30 20:05:20 +0800

    Create multiple pages using react-router

--- ecommerce-project/src/main.jsx
+++ ecommerce-project/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="checkout" element={<div>Test Checkout Page</div>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
