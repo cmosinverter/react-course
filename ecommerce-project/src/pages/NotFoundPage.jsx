76e7827857a8e1f772d372a38ed7eddcb7f6018d
kevin <kevin95120@gmail.com>
2025-07-31 14:36:15 +0800

    Refactor NotFoundPage component: simplify structure, update class names, and enhance styling

--- ecommerce-project/src/pages/NotFoundPage.jsx
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -5,31 +5,15 @@ export function NotFoundPage() {
     return (
         <>
             <Header />
-            <section className="page_404">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-12 ">
-                            <div className="col-sm-10 col-sm-offset-1  text-center">
-                                <div className="four_zero_four_bg">
-                                    <h1 className="text-center ">404</h1>
-
-
-                                </div>
-
-                                <div className="contant_box_404">
-                                    <h3 className="h2">
-                                        Look like youre lost
-                                    </h3>
-
-                                    <p>the page you are looking for not avaible!</p>
-
-                                    <Link to="/" className="link_404">Go to Home</Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <div className="notfound-page">
+                <div className="four-o-four">404</div>
+                <img className="four-o-four-bg" src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif" />
+                <p className="lost-message">Looks like you are lost</p>
+                <p className="lost-message">The page you are looking is unavailable</p>
+                <Link to="/" className="back-home-link">
+                    Back to home
+                </Link>
+            </div>
         </>
     );
 }
